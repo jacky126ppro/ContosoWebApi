8bd6f102a5f089449e39ffca2bc9d687060a2c73
昱安 陳 <jacky126pro@msn.com>
2020-05-20 22:01:06 +0800

    Revert "請用 Raw SQL Query 的方式查詢 vwDepartmentCourseCount 檢視表的內容"
    This reverts commit 46ecd6e0b276c57f4b9c228f1cc05666d06f9db7.

--- ContosoWebApi/Controllers/DepartmentsController.cs
+++ ContosoWebApi/Controllers/DepartmentsController.cs
@@ -136,11 +136,5 @@ namespace ContosoWebApi.Controllers
         {
             return _context.Department.Any(e => e.DepartmentId == id);
         }
-
-        [HttpGet("GetDepartCount")]
-        public async Task<ActionResult<IEnumerable<VwDepartmentCourseCount>>> GetDepartCount()
-        {
-          return await _context.VwDepartmentCourseCount.FromSqlRaw("select * from VwDepartmentCourseCount").ToListAsync(); ;
-        }
     }
 }
